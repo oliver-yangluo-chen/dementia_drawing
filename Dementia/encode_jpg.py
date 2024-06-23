b25a03740a79663792ba27ffec4f80990c1ded00
oliver-yangluo-chen <142708633+oliver-yangluo-chen@users.noreply.github.com>
2024-06-23 01:35:15 -0700

    Delete Dementia directory

--- Dementia/encode_jpg.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-file_path = "encoded.txt"
-
-with open("test_img.jpg", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
-    with open(file_path, 'w') as file:
-        file.write(encoded_string)
